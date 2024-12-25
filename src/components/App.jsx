49a2212166edf85cdadbda0e9a44d2eacc575ff7
Danik2023 <danik.kyrteu2@gmail.com>
2024-12-25 16:06:29 +0200

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 import { Statistics } from './Statistics';
+import { FeedbackOptions } from './Feedback/FeedbackOptions';
+import { Section } from './Section/Section';
 
 export class App extends Component {
   state = {
@@ -32,18 +34,16 @@ export class App extends Component {
     const positivePercentage = this.countPositiveFeedbackPercentage();
     return (
       <div>
-        <h1>Please leave feedback</h1>
-        <button onClick={() => this.onHandleClick('good')}>Good</button>
-        <button onClick={() => this.onHandleClick('neutral')}>Neutral</button>
-        <button onClick={() => this.onHandleClick('bad')}>Bad</button>
-
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={positivePercentage}
-        />
+        <Section title="Please leave feedback">
+          <FeedbackOptions onLeaveFeedback={this.onHandleClick} />
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
       </div>
     );
   }
