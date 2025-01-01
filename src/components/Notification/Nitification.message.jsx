04bb6acc9c3c7306867b24389c77222fd92ace8a
Danik2023 <danik.kyrteu2@gmail.com>
2025-01-01 15:22:15 +0200

    added notification message

--- src/components/Notification/Nitification.message.jsx
+++ src/components/Notification/Nitification.message.jsx
@@ -1,10 +1,5 @@
-import PropTypes from 'prop-types';
 import { NotificationMessage } from './Notification.Styled';
 
 export const Notification = ({ message }) => {
   return <NotificationMessage>{message}</NotificationMessage>;
-};
-
-Notification.propTypes = {
-  message: PropTypes.string.isRequired,
 };
